753fa09f4101694994bb03bd54f13c086a0c6a65
rachel.shadoan <rachel.shadoan@0e7eb164-5fa5-11de-a55d-e76e375ae706>
2009-07-07 18:59:26 +0000

    Here is a simple server! It's basically a MUCH pared down version of the SensorBase--all it does is say "Hello, World" when you ping it. I am not 100% sure that I took out everything except what was necessary, but I took out as much as possible.

---
+++ hackystat-analysis-socnet/SimpleServer/src/org/hackystat/sensorbase/resource/helloping/HelloPingResource.java
@@ -0,0 +1,21 @@
+package org.hackystat.sensorbase.resource.helloping;
+
+import org.hackystat.sensorbase.resource.sensorbase.SensorBaseResource;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.Variant;
+
+public class HelloPingResource extends SensorBaseResource{
+
+	public HelloPingResource(Context context, Request request, Response response) {
+	    super(context, request, response);
+	}
+	
+	public Representation represent(Variant variant) {
+		return new StringRepresentation("Hello, World");
+	}
+
+}
