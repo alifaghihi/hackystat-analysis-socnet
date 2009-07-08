9550977ff80a4caaa0993a3d65af37c0d815cb2c
rachel.shadoan <rachel.shadoan@0e7eb164-5fa5-11de-a55d-e76e375ae706>
2009-07-08 04:18:56 +0000

    Also an empty twitter class.

---
+++ trunk/src/org/hackystat/socnet/sensors/twitterclient/TwitterClient.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.hackystat.socnet.sensors.twitterclient;
+
+/**
+ *
+ * @author Rachel Shadoan
+ */
+public class TwitterClient {
+
+}
