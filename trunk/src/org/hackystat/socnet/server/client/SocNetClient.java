9a13ae06cdfa9e9496baca52a9e2fc4b72481a1e
rachel.shadoan <rachel.shadoan@0e7eb164-5fa5-11de-a55d-e76e375ae706>
2009-07-08 04:18:15 +0000

    Added a client interface and a mostly empty class implementing the client interface.

---
+++ trunk/src/org/hackystat/socnet/server/client/SocNetClient.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.hackystat.socnet.server.client;
+
+import org.hackystat.socnet.socialmediagraph.nodes.NodeFactory.IsARelationshipType;
+
+/**
+ *
+ * @author Rachel Shadoan
+ */
+public class SocNetClient implements SocNetClientInterface {
+
+    public void addNode(String nodeName, IsARelationshipType nodeType)
+    {
+        //yet to be implemented
+    }
+
+    public void addRelationshipTo(String startNodeName, IsARelationshipType startNodeType, String endNodeName, IsARelationshipType endNodeType)
+    {
+        
+    }
+
+}
