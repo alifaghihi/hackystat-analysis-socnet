2742aa867ad88ac91a29f441a41297482b867732
rachel.shadoan <rachel.shadoan@0e7eb164-5fa5-11de-a55d-e76e375ae706>
2009-07-09 21:14:39 +0000

    Changing the TwitterPoller to use a caching method that will take 4n time as opposed to n^2.

--- src/org/hackystat/socnet/sensors/twitterclient/TwitterPoller.java
+++ src/org/hackystat/socnet/sensors/twitterclient/TwitterPoller.java
@@ -2,16 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.hackystat.socnet.sensors.twitterclient;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import org.hackystat.socnet.server.client.SocNetClient;
 import org.hackystat.socnet.socialmediagraph.nodes.NodeFactory.IsARelationshipType;
@@ -24,115 +24,94 @@ import twitter4j.User;
  *
  * @author Rachel Shadoan
  */
-public class TwitterPoller {
-    
-    public enum TwitterRelationships implements RelationshipType{IS_FOLLOWED_BY, IS_FOLLOWING,
-        MENTIONED};
+public class TwitterPoller
+{
 
+    public enum TwitterRelationships implements RelationshipType
+    {
+
+        IS_FOLLOWED_BY, IS_FOLLOWING,
+        MENTIONED
+    };
     private final String clientUsername;
-    
     private final String clientPassword;
-    
     private int allowableAPICallsPerHour;
-    
     private int apiCallsMadeThisHour;
-    
     private Twitter twitter;
-    
     private ArrayList<User> users;
-    
-    private HashMap<User, List<User>> usersToFollowers;
-    
-    private HashMap<User, List<User>> usersToPeopleTheyFollow;
-    
+    private HashSet<String> usersInDB;
+    private HashMap<String, List<User>> usersToFollowers;
+    private HashMap<String, List<User>> usersToPeopleTheyFollow;
     private SocNetClient socnetclient;
-    
-    public TwitterPoller(String propertiesFile) throws FileNotFoundException, IOException, TwitterException
+
+    public TwitterPoller(String propertiesFile) throws FileNotFoundException, IOException, TwitterException, HitApiCallLimitException
     {
         BufferedReader br = new BufferedReader(new FileReader(propertiesFile));
-        
+
         clientUsername = br.readLine();
         clientPassword = br.readLine();
         allowableAPICallsPerHour = Integer.parseInt(br.readLine());
-        
-        apiCallsMadeThisHour = 0;
-        
+
         twitter = new Twitter(clientUsername, clientPassword);
-        
+
         checkAPIcalls();
-        
-        usersToPeopleTheyFollow = new HashMap<User, List<User>>();
-        usersToFollowers = new HashMap<User, List<User>>();
-      
+
+        usersToPeopleTheyFollow = new HashMap<String, List<User>>();
+        usersToFollowers = new HashMap<String, List<User>>();
+
         socnetclient = new SocNetClient();
-        
+
         ArrayList<String> twitterAcctsInDB = socnetclient.getNodes(
                 IsARelationshipType.IS_TWITTER_ACCOUNT);
-        
-        for(String twitterScreenName : twitterAcctsInDB)
+
+        for (String twitterScreenName : twitterAcctsInDB)
         {
-            users.add(twitter.getUserDetail(twitterScreenName));
-            
-            checkAPIcalls();
+            usersInDB.add(twitterScreenName);
         }
-       
+
+        ArrayList<User> currentUsers = (ArrayList<User>) twitter.getFollowers();
+        ArrayList<String> toBeDataBased = new ArrayList<String>();
+        ArrayList<String> toHaveRelationshipsTerminated = new ArrayList<String>();
+        //in a moment we will use this to see if any of our followers have been
+        //deleted
+        HashSet<String> currentUsersHash = new HashSet<String>();
+        String username;
+        for (User follower : currentUsers)
+        {
+            username = follower.getScreenName();
+            currentUsersHash.add(username);
+            if (!usersInDB.contains(username))
+            {
+                toBeDataBased.add(username);
+            }
+        }
+
+        Iterator it = usersInDB.iterator();
+
+        while(it.hasNext())
+        {
+            username = (String) it.next();
+            if(!currentUsersHash.contains(username))
+                toHaveRelationshipsTerminated.add(username);
+        }
+
 
     }
-        
-           
-        public synchronized void checkAPIcalls()
+
+    public synchronized void checkAPIcalls() throws HitApiCallLimitException, TwitterException
+    {
+        if (apiCallsMadeThisHour == (allowableAPICallsPerHour - 5))
         {
-            if(apiCallsMadeThisHour == (allowableAPICallsPerHour - 5))
-                throw new HitApiCallLimitException(apiCallsMadeThisHour, 
-                        allowableAPICallsPerHour - apiCallsMadeThisHour);
-            
-            else
-                apiCallsMadeThisHour++;
-            
+            throw new HitApiCallLimitException(apiCallsMadeThisHour,
+                    allowableAPICallsPerHour - apiCallsMadeThisHour);
         }
-        
-        public void addSocNetUsers() throws TwitterException
+        else
         {
-            String userScreenName;
-            ArrayList<User> currentUsers;
-            
-            currentUsers = (ArrayList<User>) twitter.getFollowers();
-            
-            //if this is not the initalization adding of the SocNet users and
-            //the two lists are equal
-            if(!isInitialization && users.equals(currentUsers))
-            {
-                //get the heck out of the method
-                return;
-            }
-                
-            else
-            for(User newUser : currentUsers)
-            {
-                userScreenName = newUser.getScreenName();
-            
-                if(isInitialization)
-                    socnetclient.addNode(userScreenName, 
-                        IsARelationshipType.IS_TWITTER_ACCOUNT);
-               
-               
-            }
-            
+            apiCallsMadeThisHour = twitter.rateLimitStatus().getRemainingHits();
         }
-        
-        public void addFollowersOfSocNetUser(boolean isInitialization) throws TwitterException
-        {
-            if(isInitialization)
-            {
-                users = twitter.getFollowers();
-            }
-        }
-        
-        public void addPeopleSocNetUserFollows(boolean isInitialization)
-        {
-            
-        }
+
     }
+}
     
     
     
