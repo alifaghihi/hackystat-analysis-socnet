c309873873939ed30c933e9658a8b17b6328d718
rachel.shadoan <rachel.shadoan@0e7eb164-5fa5-11de-a55d-e76e375ae706>
2009-07-10 06:18:39 +0000

    The TwitterClient class is now ready to roll, as well. Currently in the system error handling is minimal.

--- trunk/src/org/hackystat/socnet/sensors/twitterclient/TwitterClient.java
+++ trunk/src/org/hackystat/socnet/sensors/twitterclient/TwitterClient.java
@@ -5,10 +5,46 @@
 
 package org.hackystat.socnet.sensors.twitterclient;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import twitter4j.TwitterException;
+
 /**
  *
  * @author Rachel Shadoan
  */
 public class TwitterClient {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException, TwitterException, InterruptedException
+    {
+ 
+        //create the twitterPoller object
+        TwitterPoller twitterPoller = new TwitterPoller(args[0]);
+        
+        //begin polling!
+        while(true)
+        {
+            try
+            {
+                //update the database with information from twitter, if anything
+                //is new and different
+                twitterPoller.update();
+                
+                //wait for an hour before doing it again
+                twitterPoller.wait(3600000);
+            }
+            catch (HitApiCallLimitException ex)
+            {
+                //if we've hit the API limit, print out the error message
+                System.out.println(ex.getMessage());
+                System.out.println("Sleeping now.");
+                
+                //and then go to sleep.
+                twitterPoller.wait(3600000);
+            }
+        }
+    }
 
 }
